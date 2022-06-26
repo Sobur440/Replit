d603847112f6e8274dd49ed4910aff9e8ac86cc3
Sobur440 <104511346+Sobur440@users.noreply.github.com>
2022-06-26 18:21:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let Ham1 = document.querySelector(".hamburger")
 let Ham2 = document.querySelector(".ham2")
 let barOpen = document.querySelector(".bar-open")
 let codeBox = document.querySelector(".code")
-
+let width = screen.width
 
 Ham1.addEventListener("click", show)
 Ham2.addEventListener("click", disappear)
@@ -21,4 +21,9 @@ function disappear() {
 
 function poof() {
   barOpen.classList.remove("active")
+}
+function page() {
+  if (width >= 1400) {
+    alert("This website was only created for phones so its not responsive so use your phone")
+  }
 }
